9fed2e67d063e7e54d34f4f901ac6fe1f9b114ec
daniel <daaniel3232@gmail.com>
2020-08-06 12:56:35 -0300

    exercicios do cap03

---
+++ cap_03/3.7 - Reduzindo a lista de convidados.py
@@ -0,0 +1,7 @@
+lista = ('Daniel', 'Monica', 'Walter', 'Jonas', 'Raquel', 'Rubens')
+print('{} desculpe mas a mesa maior que estava reservada não está mais disponível\n'
+      '{} desculpe mas a mesa maior que estava reservada não está mais disponível\n'
+      '{} desculpe mas a mesa maior que estava reservada não está mais disponível\n'
+      '{} desculpe mas a mesa maior que estava reservada não está mais disponível\n'
+      '{} desculpe mas a mesa maior que estava reservada não está mais disponível\n'
+      '{} desculpe mas a mesa maior que estava reservada não está mais disponível\n'.format(lista[0], lista[1], lista[2],lista[3], lista[4], lista[5]))
